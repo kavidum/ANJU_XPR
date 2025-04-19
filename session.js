f318c3e0eaa500325f41d0ff3786f2f9ae9b661b
Kavidumadu <206985060+Kavidumadu@users.noreply.github.com>
2025-04-19 13:42:36 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~vNkFXL4K#P4N4uPvaw0Ig4-8mJnxaRtGunh9maCbxd9OplWNsBNE",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
